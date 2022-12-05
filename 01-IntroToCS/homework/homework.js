2227c48084b507033aea27106617f3df235f01f6
Diegoc24 <carusodiegod@gmail.com>
2022-12-05 17:21:52 -0300

    bin

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,43 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let it = 0;
+   let decimal = 0;
+   let suma = 0;
+   for(i = num.length -1; i > -1;i--){
 
-function DecimalABinario(num) {}
+      decimal = (num[i] * (2 ** it));
+
+      it += 1;
+
+      suma = suma += decimal;
+
+   }
+
+    return suma;
+}
+
+function DecimalABinario(num) {
+   let numero = num;
+      let boo = false;
+      let resto = 0;
+      let bin = [];
+   for(i = 0;numero > i;i++){
+  
+     
+  
+    
+      
+      resto = numero % 2;
+      numero = numero / 2;
+      numero =  Math.round(numero);
+      console.log(resto);
+   
+
+   bin.push(resto);
+   }
+   return bin;
+}
 
 module.exports = {
    BinarioADecimal,
